52f45d074e151721c43ec90ddfeb1a0c71150c7d
1111MdTanvirAhmed1111 <freelancertanvir91@gmail.com>
2024-12-02 14:12:19 +0600

    Update multer.js

--- middlewares/multer.js
+++ middlewares/multer.js
@@ -1,9 +1,8 @@
 const multer  = require('multer')
-const path = require('path')
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-      cb(null, path.join(__dirname, '/uploads/'));
+      cb(null, './uploads')
     },
     filename: function (req, file, cb) {
  
