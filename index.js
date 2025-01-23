06af715476431d827f6e330654ecbdd4d082cff1
1111MdTanvirAhmed1111 <freelancertanvir91@gmail.com>
2025-01-24 03:59:06 +0600

    Update index.js

--- index.js
+++ index.js
@@ -4,16 +4,16 @@ const router = require('./router')
 const { default: mongoose } = require('mongoose')
 const app = express()
 const cors = require('cors')
-var http = require('http');
+var https = require('https');
 
 app.use(cors())
 app.use(express.json({limit:"50mb"}))
 app.use('/', router)
 
 setInterval(() => {
-http.get("https://ecommerce-website-backend-lrot.onrender.com"); // Make a request to your app
-http.get("https://react-ecommerce-backend-a9y3.onrender.com")
-}, 40 * 1000);
+https.get("https://ecommerce-website-backend-lrot.onrender.com"); // Make a request to your app
+https.get("https://react-ecommerce-backend-a9y3.onrender.com")
+}, 2* 60 * 1000);
 
 app.listen(process.env.PORT)
 mongoose.connect(process.env.mongoDB).then(()=>console.log("connected")).catch(err=>console.log(err))
