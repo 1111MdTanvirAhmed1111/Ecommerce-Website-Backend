79788eef45dd7153ea5e054263ea8b368a0cd640
1111MdTanvirAhmed1111 <freelancertanvir91@gmail.com>
2025-03-12 20:52:08 +0600

    Update index.js

--- index.js
+++ index.js
@@ -10,10 +10,6 @@ app.use(cors())
 app.use(express.json({limit:"50mb"}))
 app.use('/', router)
 
-setInterval(() => {
-http.get("https://ecommerce-website-backend-lrot.onrender.com"); // Make a request to your app
-http get("https://react-ecommerce-backend-a9y3.onrender.com")
-}, 2* 60 * 1000);
 
 app.listen(process.env.PORT)
 mongoose.connect(process.env.mongoDB).then(()=>console.log("connected")).catch(err=>console.log(err))
